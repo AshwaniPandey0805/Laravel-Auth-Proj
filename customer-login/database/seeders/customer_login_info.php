2acfcb00434a1c9c95af489122945498f60f2e1d
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-02-21 14:39:53 +0530

    added seeder functionality. File, customer_login_info.php, to inset dummy data for the client.

---
+++ customer-login/database/seeders/customer_login_info.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class customer_login_info extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $currentDateTime = now();
+        $currentTime = $currentDateTime->format('H:i:s');
+        DB::table('customer_login_info')->insert([
+            "name" => Str::random(10),
+            "email" => Str::random(10)."@gmail.com",
+            "password" => Hash::make(Str::random(10)),
+            "created_at" => $currentTime,
+            "updated_at" => $currentTime,
+            "email_verified_at" => $currentTime,
+
+        ]);
+    }
+}
