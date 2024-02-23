69f760368c81497e06d5fa89f13533fbb57af548
Ashwani Kumar Pandey <ashwanipandey679@gmail.com>
2024-02-23 12:14:19 +0530

    created customer_login_register_table in database

--- customer-login/database/migrations/2024_02_23_063514_register_pannel.php
+++ customer-login/database/migrations/2024_02_23_063514_register_pannel.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customer_login_info', function (Blueprint $table) {
+        Schema::create('custome_login_register_table', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-    
     }
 
     /**
